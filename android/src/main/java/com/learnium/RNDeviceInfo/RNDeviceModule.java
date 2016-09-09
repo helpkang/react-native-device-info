98297d3d5087730b3cfaf9f5fc2d8bb1553820b5
helpkang <helpkang@gmail.com>
2016-09-09 18:20:10 +0900

    Update RNDeviceModule.java
    폰번호 추가 phoneNumber

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -85,6 +85,7 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
     constants.put("systemVersion", Build.VERSION.RELEASE);
     constants.put("model", Build.MODEL);
     constants.put("deviceId", mTelephonyMgr.getDeviceId());
+    constants.put("phoneNumber", mTelephonyMgr.getLine1Number());
     constants.put("deviceLocale", this.getCurrentLanguage());
     constants.put("deviceCountry", this.getCurrentCountry());
     constants.put("uniqueId", Secure.getString(this.reactContext.getContentResolver(), Secure.ANDROID_ID));
